b7b5de6af5f4766c1ad0f522d4b6c57f4cc71a67
himitu23 <himitu23centcircumflex@gmail.com>
2014-08-15 04:54:24 +0900

    innnit commit

---
+++ Review/src/jp/ac/hiroshimacu/test1/Extern_database.java
@@ -0,0 +1,62 @@
+package jp.ac.hiroshimacu.test1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.sql.*;
+
+/**
+ * Servlet implementation class extnern_databse
+ */
+@WebServlet("/extnern_databse")
+public class Extern_database extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Extern_database() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>ìäçeÉtÉHÅ[ÉÄ</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("aaaa");
+        out.println("</body>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        
+	}
+
+}
---
+++ Review/src/jp/ac/hiroshimacu/test1/delete_page.java
@@ -0,0 +1,51 @@
+package jp.ac.hiroshimacu.test1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class delete_page
+ */
+@WebServlet("/delete_page")
+public class delete_page extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public delete_page() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>ìäçeÉtÉHÅ[ÉÄ</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("aaaa");
+        out.println("</body>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
